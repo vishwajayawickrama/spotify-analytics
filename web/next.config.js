3e9143c273ee878a4d2809a7e80428d2b210bba6
vishwajayawickrama <pmvishwajayawickrama@gmail.com>
2026-04-14 16:11:38 +0530

    Fix: Update build command in GitHub Actions workflow and adjust Next.js configuration for export output

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "i.scdn.co" },
       { protocol: "https", hostname: "mosaic.scdn.co" },
